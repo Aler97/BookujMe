5bb9d423d369188f4cbd9cb649077e88f511f852
tijanadragnic <tijanadragnic98@gmail.com>
2022-03-05 21:11:04 +0100

    oglasi

--- src/Components/Oglas.js
+++ src/Components/Oglas.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext,useState,useEffect } from 'react';
 import { Link as ReactLink } from 'react-router-dom'
 import books from '../Images/Books-High-Quality-PNG.png'
 import {
@@ -11,13 +11,21 @@ import {
 
 } from '@chakra-ui/react';
 import { UserIdContext } from '../helpers/userIdContext';
+import axios from 'axios';
 
 
-const Oglas = () => {
+    
+
+const Oglas = ({id,name,author_firstname,author_lastname,genre,preservation_level}) => {
     const { userId } = useContext(UserIdContext);
 
+   
+   
 
-    return (<><SimpleGrid
+   
+
+    return (<>
+     <SimpleGrid
         columns={{ sm: 1, md: 1 }}
         w='100%'
         h='auto'
@@ -25,18 +33,18 @@ const Oglas = () => {
         borderColor='black'
         borderRadius='0.5em'
     ><Img src={books} alt='books'></Img>
-        <Flex flexDirection='column' mb='15px' paddingLeft='10px' gap='5px'>
-            <Text marginTop='10px'><b>Naziv Knjige:</b></Text>
-            <Text><b>Autor:</b></Text>
-            <Text><b>Očuvanost:</b></Text>
-            <Text><b>Edicija:</b></Text>
-            <Link as={ReactLink} to={`/Oglasi/${userId}`} alignSelf='center' marginTop='10px'><Button bgColor='button.normal'
-                color='white'
-                fontWeight='bold'
+    <Flex flexDirection='column' mb='15px' paddingLeft='10px' gap='5px'>
+        <Text marginTop='10px'><b>Naziv Knjige: {name}</b></Text>
+        <Text><b>Autor: {author_firstname} {author_lastname}</b></Text>
+        <Text><b>Očuvanost: {preservation_level}</b></Text>
+        <Text><b>Zanr: {genre}</b></Text>
+        <Link as={ReactLink} to={`/Oglasi/${userId}`} alignSelf='center' marginTop='10px'><Button bgColor='button.normal'
+            color='white'
+            fontWeight='bold'
 
-                _hover={{ bgColor: 'black', color: 'white' }} _active={{ bgColor: 'black', color: 'white' }}>Opširnije</Button></Link>
+            _hover={{ bgColor: 'black', color: 'white' }} _active={{ bgColor: 'black', color: 'white' }}>Opširnije</Button></Link>
 
-        </Flex>
+    </Flex>
 
     </SimpleGrid></>);
 }
